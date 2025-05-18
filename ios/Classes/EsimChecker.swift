63f888d72aeb096c571ed327d80168305d6f2476
asadamatic <asadamatic@gmail.com>
2025-05-18 12:17:16 +0500

    Fixed the isSupportEsim method.

--- ios/Classes/EsimChecker.swift
+++ ios/Classes/EsimChecker.swift
@@ -29,11 +29,20 @@ class EsimChecker: NSObject {
     public var handler: EventCallbackHandler?;
     
     
-    public var identifier: String = "iPhone17,1"
+    public var identifier: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let mirror = Mirror(reflecting: systemInfo.machine)
+        
+        let identifier = mirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
+    }()
     
     func isSupportESim() -> Bool {
-
-  
+        
         if identifier.hasPrefix("iPhone") {
             return isModelNumberGreaterOrEqual(identifier, reference: minSupportediPhone)
         } 
